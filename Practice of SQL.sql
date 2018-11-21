631c059ae2f1b44fc75fe168ac0295c5f701e5d2
Li Cheng-En <datotogood@icloud.com>
2018-11-21 11:05:26 +0800

    Add more codes about selection and ordering of data.

--- Practice of SQL.sql
+++ Practice of SQL.sql
@@ -1,11 +1,31 @@
-# Extract
+# Extract Whole Data from Form & Other Form
 select * from city;
+select * from cmdev.emp;
 
-SELECT Name, District, Population FROM city;
+# Extract Whole Data with Particular Columns from Form
+select Name, District, Population from city;
 
-#SELECT population*12 AS AnnualPayment FROM city
+# Extract & Transform & Rename the Data
+select population*12 as AnnualPayment from city;
 
-SELECT * FROM city WHERE Population > 100000;
-SELECT * FROM city WHERE Population BETWEEN 10000 AND 100000
+# Extract Whole Data with Particular Conditions about Integer from Form
+select * from city where Population > 100000;
+select * from city where Population between 10000 and 100000;
 
-#SELECT job FROM cmdev.emp
+# Extract Whole Data with Particular Conditions about String from Form
+select * from cmdev.emp where ename like 'S%';     # "like" means "data could include", "%" means " there could be some characters after the designated character" (Here is "S").
+select * from cmdev.emp where ename like '%N%';  
+
+# Extract Null Data & Non-null Data
+select * from cmdev.emp where comm is null;
+select * from cmdev.emp where comm is not null;
+
+# Order the Data
+select * from city order by Population desc;     #  "asc" means "ascending", and "desc" means "descending".
+select * from city order by Population asc, CountryCode asc; 
+
+# Limit the Number of Data We Extracted
+select * from cmdev.emp order by salary asc limit 5;
+
+# Excluded The Same Data
+select distinct * from cmdev.emp order by manager;
