61f18f699301d59316983b74c53e476adb1167a7
Li Cheng-En <datotogood@icloud.com>
2018-11-26 07:31:55 +0800

    Add mode codes about fundanebtal function.

--- Practice of SQL.sql
+++ Practice of SQL.sql
@@ -38,6 +38,37 @@ select distinct * from cmdev.emp order by manager;
 
 
 
-# Obtain Particular Part From String & Length of String
+# Obtain Particular Part From String & Length of String & Time
 select substring("Advertisement", 2, 8);
-select length(District) from world.city;
+select char_length(District) from world.city;
+select hiredate + interval 36 day from cmdev.emp limit 10 ;
+
+# Function of String
+select ename, lower(ename) anotherName from cmdev.emp;     # lower() is a function which return the lower case of characters.
+select ename, replace(ename, "S", "PP") from cmdev.emp;     # replace() is a function which could be used to change part of string.
+
+# Function of Number
+select 3.141592653, round(3.141592653), ceil(3.141592653), truncate(3.141592653, 4);      # Rounded with particular rules.
+select pi(), rand(), sqrt(25);      # pi() is used to generate 3.1415~, rand() is used to generate a number which is between 0 and 1, and sqrt() is used to generate the root of particular number.alter
+
+# Function of Date
+select ename, hiredate, datediff('1988-01-01', hiredate) days from cmdev.emp;     # datediff() is a function which returns the days between two dates, there're two parameters in it.
+select curdate(), month(curtime()), day(curtime()), hour(curtime()), minute(curtime());     # Return the time right now.
+select Name, year(curdate()) - IndepYear  as LengthOfYearOfIndep from world.country;      # Year() is used to extract the year of particular date.
+select "2018-06-25" as birthday, adddate("2018-06-25", 20), subdate("2018-06-25", 20);
+
+# Control Flow-- If (If in Swift) & Case (Switch in Swift)
+select Name, Population, if(Population > 1000000, "Crowded", "Better") Situation from world.country;      # if(condition, case1, case 2)
+select Name, GNP, case
+	when GNP < 1000 then "Poor"
+    when GNP < 5000 and GNP >= 1000 then "Medium"
+    else "Rich"
+end PropertySituation from world.country;
+
+# Distinguish Null
+select Name, isnull(IndepYear) from world.country;      # 0 means not null, and vice versa.
+select Name, ifnull(IndepYear, "Empty") Checked from world.country;
+
+
+select * from world.country;
+
